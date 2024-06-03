58268b4fd71cf7585d89fb820a0ced3f8bcf6b8f
Note-m <ant.myro.shop@gmail.com>
2024-06-03 00:11:52 -0700

    fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { buttonFaq } from './js/faq.js';
 import { manageModal } from './js/work-together.js';
+import './js/reviews.js';
+import './js/hero.js';
+import './js/my-projects.js';
 buttonFaq();
 manageModal();
-import './js/reviews.js';
-import './js/my-projects.js';

ff1b48b8411ebbbaa22ab31748bea474145c3ba6
olgafedorec <olga_fedorec@yahoo.com>
2024-06-03 10:36:54 +0300

    update header.css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { buttonFaq } from './js/faq.js';
 import { manageModal } from './js/work-together.js';
+import { onSwitchTheme } from './js/switch-button.js'
 buttonFaq();
 manageModal();
 import './js/reviews.js';
 import './js/my-projects.js';
+onSwitchTheme();

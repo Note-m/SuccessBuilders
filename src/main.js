5d9192bce46ba0c7ebb4e12925d2f286b50f7ee2
Note-m <ant.myro.shop@gmail.com>
2024-06-02 21:08:14 -0700

    +

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// import { buttonFaq } from './js/faq.js';
-// import { manageModal } from './js/work-together.js';
-// buttonFaq();
-// manageModal();
-// import './js/my-projects.js';
+import { buttonFaq } from './js/faq.js';
+import { manageModal } from './js/work-together.js';
+buttonFaq();
+manageModal();
+import './js/my-projects.js';

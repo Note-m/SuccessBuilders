719989bcc86717dac837c20d933f50d426d538cf
Nataliia21a <152820534+Nataliia21a@users.noreply.github.com>
2024-06-03 14:22:31 +0300

    way to img test

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { onSwitchTheme } from './js/switch-button.js'
+import { onSwitchTheme } from './js/switch-button.js';
 import { buttonFaq } from './js/faq.js';
 import { manageModal } from './js/work-together.js';
 import './js/reviews.js';
 import './js/hero.js';
-import './js/my-projects.js';
+import './js/my-projects';
 buttonFaq();
 manageModal();
 onSwitchTheme();

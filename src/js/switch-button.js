0c8bd4dd35aa1ae1c0dc74723cda21b0a5e39d19
AliceMozyrko <mozyrkoalisa@gmail.com>
2024-06-04 10:41:35 +0300

    update

--- src/js/switch-button.js
+++ src/js/switch-button.js
@@ -1,9 +1,88 @@
 const switchThemeBtnEl = document.querySelector('.switch-btn');
-
 export const onSwitchTheme = event => {
 switchThemeBtnEl.classList.toggle('dark-on');
 document.body.classList.toggle('body-dark'); 
 } 
+  switchThemeBtnEl.classList.toggle('dark-on');
+  document.body.classList.toggle('body-dark');
+
+  const icons = document.querySelectorAll('.use-acardion');
+  const upIconsSkills = document.querySelectorAll('.up-line-icon use');
+  const downIconsSkills = document.querySelectorAll('.down-line-icon use');
+  const arrowRevPrev = document.querySelectorAll('.btn-prev use');
+  const arrowRevNext = document.querySelectorAll('.btn-next use');
+  const iconPrev = prevBtn.querySelector('.icon-swipe-prev use');
+  const iconNext = nextBtn.querySelector('.icon-swipe-next use');
+
+  icons.forEach(icon => {
+    let iconName;
+    if (document.body.classList.contains('body-dark')) {
+      iconName = 'faq-arrow-white-down-icon';
+    } else {
+      iconName = 'faq-arrow-down-icon';
+    }
+    icon.setAttribute('href', `${iconsUrl}#${iconName}`);
+  });
+
+  downIconsSkills.forEach(icon => {
+    let iconSkillsName;
+    if (document.body.classList.contains('body-dark')) {
+      iconSkillsName = 'tech-skills-dark-white-square-icon';
+    } else {
+      iconSkillsName = 'tech-skills-black-square-icon';
+    }
+    icon.setAttribute('href', `${iconsUrl}#${iconSkillsName}`);
+  });
+
+  upIconsSkills.forEach(icon => {
+    let iconSkillsName;
+    if (document.body.classList.contains('body-dark')) {
+      iconSkillsName = 'tech-skills-dark-green-square-icon';
+    } else {
+      iconSkillsName = 'tech-skills-green-square-icon';
+    }
+    icon.setAttribute('href', `${iconsUrl}#${iconSkillsName}`);
+  });
+
+
+  // --------------- REVIEWS --------------- // 
+    // arrowRevPrev.forEach(icon => {
+    //   const parentButtonPrev = document.querySelector('.arrow-revi-prev');
+    //   let arrowRevPrevName;
+    //   if (document.body.classList.contains('body-dark')) {
+    //     if (parentButtonPrev.disabled) {
+    //       arrowRevPrevName = 'reviews-dark-grey-btn-prev';
+    //     } else {
+    //       arrowRevPrevName = 'reviews-white-btn-prev';
+    //     }
+    //   } else {
+    //     if (parentButtonPrev.disabled) {
+    //       arrowRevPrevName = 'reviews-grey-btn-prev';
+    //     } else {
+    //       arrowRevPrevName = 'reviews-black-btn-prev';
+    //     }
+    //   }
+    //   icon.setAttribute('href', `${iconsUrl}#${arrowRevPrevName}`);
+    // });
+
+    // arrowRevNext.forEach(icon => {
+    //   const parentButtonNext = document.querySelector('.arrow-revi-next');
+    //   let arrowRevNextName;
+    //   if (document.body.classList.contains('body-dark')) {
+    //     if (parentButtonNext.disabled) {
+    //       arrowRevNextName = 'reviews-dark-grey-btn-next';
+    //     } else {
+    //       arrowRevNextName = 'reviews-white-btn-next';
+    //     }
+    //   } else {
+    //     if (parentButtonNext.disabled) {
+    //       arrowRevNextName = 'reviews-grey-btn-next';
+    //     } else {
+    //       arrowRevNextName = 'reviews-black-btn-next';
+    //     }
+    //   }
+    //   icon.setAttribute('href', `${iconsUrl}#${arrowRevNextName}`);
+    // });
 
 switchThemeBtnEl.addEventListener('click', onSwitchTheme);
 document.addEventListener('DOMContentLoaded', () => {

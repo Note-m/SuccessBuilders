7f19f469955529c85b0d2239ca5ef81fed98c2d5
Note-m <ant.myro.shop@gmail.com>
2024-06-03 21:51:52 -0700

    Fix-dark-theme

--- src/js/switch-button.js
+++ src/js/switch-button.js
@@ -1,13 +1,23 @@
 const switchThemeBtnEl = document.querySelector('.switch-btn');
+const iconsUrl = '../img/icons.svg';
 
 export const onSwitchTheme = event => {
-switchThemeBtnEl.classList.toggle('dark-on');
-document.body.classList.toggle('body-dark');
+  switchThemeBtnEl.classList.toggle('dark-on');
+  document.body.classList.toggle('body-dark');
 
-
-} 
+  const icons = document.querySelectorAll('.use-acardion');
+  icons.forEach(icon => {
+    let iconName;
+    if (document.body.classList.contains('body-dark')) {
+      iconName = 'faq-arrow-white-down-icon';
+    } else {
+      iconName = 'faq-arrow-down-icon';
+    }
+    icon.setAttribute('href', `${iconsUrl}#${iconName}`);
+  });
+};
 
 switchThemeBtnEl.addEventListener('click', onSwitchTheme);
 document.addEventListener('DOMContentLoaded', () => {
-    onSwitchTheme();
+  onSwitchTheme();
 });
